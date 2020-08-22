55c3d335fbc2aebaf006d33f045ef995d06f902f
Abby Paul <birddog1198@gmail.com>
2020-08-22 14:48:45 -0500

    Completed Inheritance Guide

---
+++ Assets/Scripts/Collectibles/CollectibleBase.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class CollectibleBase : MonoBehaviour
+{
+    protected abstract void Collect(Player player);
+
+    [SerializeField] float _movementSpeed = 1;
+    protected float MovementSpeed { get { return _movementSpeed; } }
+
+    [SerializeField] ParticleSystem _collectParticles;
+    [SerializeField] AudioClip _collectSound;
+
+    Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement(_rb);
+    }
+
+    protected virtual void Movement(Rigidbody rb)
+    {
+        //calculate rotation
+        Quaternion turnOffset = Quaternion.Euler(0, _movementSpeed, 0);
+        rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            Collect(player);
+            //spawn particles/fx beause we need to disable object
+            CollectFeedback();
+            DisableSelf();
+        }
+    }
+
+    protected virtual void CollectFeedback()
+    {
+        //particles
+        if (_collectParticles != null)
+        {
+            Instantiate(_collectParticles, transform.position, Quaternion.identity);
+        }
+        //audio TODO - Consider Object Pooling
+        if (_collectSound != null)
+        {
+            AudioHelper.PlayClip2D(_collectSound, 1f);
+        }
+    }
+
+    protected virtual void DisableSelf()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Collectibles/SpeedIncrease.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedIncrease : CollectibleBase
+{
+    [SerializeField] float _speedAmount = 5f;
+
+    protected override void Collect(Player player)
+    {
+        BallMotor motor = player.GetComponent<BallMotor>();
+        if (motor != null)
+        {
+            motor.MaxSpeed += _speedAmount;
+        }
+    }
+
+    protected override void Movement(Rigidbody rb)
+    {
+        //calc rotation
+        Quaternion turnOffset = Quaternion.Euler(MovementSpeed, MovementSpeed, MovementSpeed);
+        rb.MoveRotation(rb.rotation * turnOffset);
+    }
+}
